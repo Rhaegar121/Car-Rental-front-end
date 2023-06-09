119a2b106a3ef58d40ecf081f77545f6fd65c355
187jjay187 <187jjay187@gmail.com>
2023-06-09 20:21:49 +0200

    Add App.js routses

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Body from './components/Body';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
