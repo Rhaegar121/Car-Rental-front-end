637fedf7ad992272a8f2da11b977199e18385b93
Jay G.O <187jjay187@gmail.com>
2023-06-12 18:43:42 +0200

    Revert "Rachel main page"

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import Body from './components/Body';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Main from './components/Main';
-import { fetchCars } from './redux/carsSlice';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCars());
-  }, [dispatch]);
-
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={<Body />}
-          />
-          <Route
-            path="/signin"
-            element={<SignIn />}
-          />
-          <Route
-            path="/signup"
-            element={<SignUp />}
-          />
-          <Route
-            path="/main"
-            element={<Main />}
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
