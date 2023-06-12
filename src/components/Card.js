62aa0c0191fa57a72a20a5eb09cad9704c65539c
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-12 17:51:07 +0400

    Add component and styles to main page

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import carImage from '../assets/carImage.jpg';
+// import '../styles/main.css';
+
+// function CarCard() {
+//   return (
+//     <Card className="main-container">
+//       <Card.Img
+//         variant="top"
+//         src={carImage}
+//         className="car-image"
+//       />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default CarCard;
