8d5710a18a679265fb41c69a84ec393ac42b98e5
187jjay187 <187jjay187@gmail.com>
2023-06-13 12:51:50 +0200

    Update Store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './carsSlice';
+import userReducer from './userSlice';
 
 const store = configureStore({
   reducer: {
     car: carsReducer,
+    user: userReducer, // Add the userReducer to the store
   },
 });
 
