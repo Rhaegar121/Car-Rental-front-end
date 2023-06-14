5787b382da5906d3d0ca6d2f1011cdd6f4cb10f7
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-15 00:31:25 +0630

    add style to detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AiOutlineStar, AiFillStar } from 'react-icons/ai';
 import { IoIosArrowBack } from 'react-icons/io';
+import '../styles/detail.css';
 
 const Detail = () => {
   const { id } = useParams();
@@ -17,37 +18,40 @@ const Detail = () => {
 
   const starIcons = Array(car.ratings)
     .fill(null)
-    .map((_, index) => <AiFillStar key={index} />);
+    .map((_, index) => <AiFillStar key={index} className="star" />);
 
   const emptyStarIcons = Array(5 - car.ratings)
     .fill(null)
     .map((_, index) => <AiOutlineStar key={index} />);
 
   return (
-    <section>
-      <header>
-        <IoIosArrowBack />
-        <h1>{car.name}</h1>
+    <section id="detail">
+      <header className="header">
+        <IoIosArrowBack className="back-btn" />
+        <h2 className="title">{car.name}</h2>
       </header>
-      <div>
-        <img src={car.image} alt={car.name} />
-        <div>
-          <div>
+      <div className="img-container">
+        <img src={car.image} alt={car.name} className="img" />
+        <div className="img-text">
+          <div className="rating">
             <p>{car.name}</p>
             <div className="star-icons">
               {starIcons}
               {emptyStarIcons}
             </div>
           </div>
-          <div>
-            <p>{car.price}</p>
+          <div className="price">
+            <p>
+              $
+              {car.price}
+            </p>
             <p>per month</p>
           </div>
         </div>
       </div>
-      <div>
-        <h3>About this car</h3>
-        <p>{car.description}</p>
+      <div className="about-container">
+        <h3 className="about">About this car</h3>
+        <p className="description">{car.description}</p>
       </div>
     </section>
   );
