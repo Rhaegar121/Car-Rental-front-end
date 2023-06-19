747cc710adb7821ee5ce3ea635963ba35b6164a3
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-19 19:00:25 +0630

    create addfavourites method in favouritesSlice.js

--- src/redux/favouritesSlice.js
+++ src/redux/favouritesSlice.js
@@ -17,6 +17,21 @@ export const fetchfavourites = createAsyncThunk(
   },
 );
 
+export const addfavourite = createAsyncThunk(
+  'cars/addfavourite',
+  async (userId, carId) => {
+    const response = await fetch(`${baseURL}/${userId}/favourites`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ car_id: carId }),
+    });
+    const data = await response.json();
+    return data;
+  },
+);
+
 const favouritesSlice = createSlice({
   name: 'favourites',
   initialState,
@@ -34,6 +49,20 @@ const favouritesSlice = createSlice({
         cars: action.payload.cars,
       }))
       .addCase(fetchfavourites.rejected, (state) => ({
+        ...state,
+        isLoading: false,
+      }))
+      .addCase(addfavourite.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(addfavourite.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        favourites: action.payload.favourites,
+        cars: action.payload.cars,
+      }))
+      .addCase(addfavourite.rejected, (state) => ({
         ...state,
         isLoading: false,
       }));
